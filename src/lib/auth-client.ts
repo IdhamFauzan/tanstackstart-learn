503054b43a7bd2e035be196ff45b7d45ccb6fd6b
IdhamFauzan <165901763+IdhamFauzan@users.noreply.github.com>
2026-01-28 21:04:57 +0700

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://3000-firebase-tanstackstart-learn-1768807531958.cluster-nulpgqge5rgw6rwqiydysl6ocy.cloudworkstations.dev"
+    baseURL: "https://tackstart-learn.vercel.app"
 })
